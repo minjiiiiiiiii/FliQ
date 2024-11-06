c0c553516ef687184022485dd5debfc5931b6b39
slo0ey <rurchi1206@gmail.com>
2024-11-07 05:51:35 +0900

    feat: bottom navigation 완성

--- app/src/main/java/com/hongul/filq/ui/BottomNavigation.kt
+++ app/src/main/java/com/hongul/filq/ui/BottomNavigation.kt
@@ -1,23 +1,113 @@
 package com.hongul.filq.ui
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-
-
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.hongul.filq.ui.theme.PrimaryDark
 
 @Composable
-fun BottomNavigation(navController: NavHostController) {
-    val items = listOf()
+fun BoxScope.BottomNavigation(navController: NavHostController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Row(
+        modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .background(Color.White)
+            .fillMaxWidth()
+            .drawWithContent {
+                drawContent()
+                drawLine(
+                    color = Color(0xFFD7D7D7),
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width, 0f),
+                    strokeWidth = 1f,
+                )
+            }
+            .padding(vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        val navItemList = listOf(
+            NavItem.Home,
+            NavItem.Contact,
+            NavItem.QR,
+            NavItem.More
+        )
 
+        for (item in navItemList) {
+            BottomNavigationItem(
+                icon = item.icon,
+                title = item.title,
+                isSelected = item.route == navBackStackEntry?.destination?.route,
+            ) {
+                navController.navigate(item.route) {
+                    navController.graph.startDestinationRoute?.let {
+                        popUpTo(it) { saveState = true }
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun BottomNavigationItem() {
+fun BottomNavigationItem(
+    @DrawableRes icon: Int,
+    title: String,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    val baseColor = if (isSelected) PrimaryDark else Color(0xFFBEBEBE)
 
+    Column(
+        modifier = Modifier
+            .size(48.dp)
+            .border(0.dp, Color.Transparent, CircleShape)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            ImageVector.vectorResource(id = icon),
+            contentDescription = title,
+            tint = baseColor
+        )
+        Text(
+            title,
+            fontWeight = FontWeight.SemiBold,
+            color = baseColor
+        )
+    }
 }
--- app/src/main/java/com/hongul/filq/ui/NavigationGraph.kt
+++ app/src/main/java/com/hongul/filq/ui/NavigationGraph.kt
@@ -1,14 +1,24 @@
 package com.hongul.filq.ui
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.hongul.filq.R
 import com.hongul.filq.ui.home.HomeScreen
 
-sealed class NavItem(val route: String, @DrawableRes val iconUnselected: Int, @DrawableRes val iconSelected: Int) {
-    data object Home : NavItem("home")
+sealed class NavItem(val route: String, val title: String, @DrawableRes val icon: Int) {
+    data object Home : NavItem("home", "내 명함", R.drawable.ic_nav_home)
+    data object Contact : NavItem("contact", "연락처", R.drawable.ic_nav_contact)
+    data object QR : NavItem("qr", "QR 공유", R.drawable.ic_nav_qr)
+    data object More : NavItem("more", "더보기", R.drawable.ic_nav_more)
 }
 
 @Composable
@@ -17,5 +27,26 @@ fun NavigationGraph(navController: NavHostController) {
         composable(NavItem.Home.route) {
             HomeScreen()
         }
+        composable(NavItem.Contact.route) {
+            PlaceHolder(it.destination.route!!)
+        }
+        composable(NavItem.QR.route) {
+            PlaceHolder(it.destination.route!!)
+        }
+        composable(NavItem.More.route) {
+            PlaceHolder(it.destination.route!!)
+        }
     }
+}
+
+@Composable
+private fun PlaceHolder(title: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        content = {
+            Text(text = title)
+        }
+    )
 }

4609c2b881af287a1ff2027a1562b226583291a4
slo0ey <rurchi1206@gmail.com>
2024-11-12 04:01:25 +0900

    feat: Avatar 업데이트 기능 추가

--- app/src/main/java/com/hongul/filq/data/BusinessCardRepository.kt
+++ app/src/main/java/com/hongul/filq/data/BusinessCardRepository.kt
@@ -1,11 +1,14 @@
 package com.hongul.filq.data
 
+import com.hongul.filq.model.Avatar
 import kotlinx.coroutines.flow.Flow
 
 interface BusinessCardRepository {
     suspend fun insert(businessCardEntity: BusinessCardEntity)
 
     suspend fun update(businessCardEntity: BusinessCardEntity)
+
+    suspend fun updateAvatar(id: Int, avatar: Avatar)
 
     suspend fun delete(businessCardEntity: BusinessCardEntity)
 
--- app/src/main/java/com/hongul/filq/data/OfflineBusinessCardRepository.kt
+++ app/src/main/java/com/hongul/filq/data/OfflineBusinessCardRepository.kt
@@ -1,11 +1,18 @@
 package com.hongul.filq.data
 
+import com.hongul.filq.model.Avatar
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 
 class OfflineBusinessCardRepository(private val dao: BusinessCardDao) : BusinessCardRepository {
     override suspend fun insert(businessCardEntity: BusinessCardEntity) = dao.insert(businessCardEntity)
 
     override suspend fun update(businessCardEntity: BusinessCardEntity) = dao.update(businessCardEntity)
+
+    override suspend fun updateAvatar(id: Int, avatar: Avatar) {
+        val card = getBusinessCard(id).first()
+        dao.update(card!!.copy(avatar = avatar))
+    }
 
     override suspend fun delete(businessCardEntity: BusinessCardEntity) = dao.delete(businessCardEntity)
 

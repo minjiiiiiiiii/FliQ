8e76536caa12aeb31a61198fcc2a968a01b92484
slo0ey <rurchi1206@gmail.com>
2024-11-11 14:15:40 +0900

    fix: modalbottomsheet가 하단 ui와 겹치던 문제 해결

--- app/src/main/java/com/hongul/filq/MainActivity.kt
+++ app/src/main/java/com/hongul/filq/MainActivity.kt
@@ -3,6 +3,10 @@ package com.hongul.filq
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.hongul.filq.ui.NavigationGraph
 import com.hongul.filq.ui.theme.FilQTheme
@@ -10,9 +14,14 @@ import com.hongul.filq.ui.theme.FilQTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             FilQTheme(dynamicColor = false) {
-                NavigationGraph(navController = rememberNavController())
+                Box(Modifier.safeDrawingPadding()) {
+                    NavigationGraph(navController = rememberNavController())
+                }
             }
         }
     }

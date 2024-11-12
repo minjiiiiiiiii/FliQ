32caf4606a05da3b67213eca99d880c9b382fa93
ddu111 <aazz8447@naver.com>
2024-11-13 01:25:24 +0900

    chore: fixing merge conflicts

--- app/src/main/java/com/hongul/filq/ui/MainActivity.kt
+++ app/src/main/java/com/hongul/filq/ui/MainActivity.kt
@@ -4,37 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.hongul.filq.FliQApp
 import com.hongul.filq.ui.theme.FilQTheme
-import com.hongul.filq.ui.AppScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            FilQTheme {
-                AppScreen()
+            FilQTheme(dynamicColor = false) {
+                Box(Modifier.safeDrawingPadding()) {
+                    FliQApp()
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FilQTheme {
-        Greeting("Android")
     }
 }

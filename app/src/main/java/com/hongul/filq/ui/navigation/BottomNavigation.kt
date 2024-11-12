3cea10644fc1a299d880277e55c19d02938fc0a7
slo0ey <rurchi1206@gmail.com>
2024-11-12 17:44:13 +0900

    chore: bottom navigation에 qr버튼 변경
    qr -> work in progress 버튼으로 변경

--- app/src/main/java/com/hongul/filq/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/hongul/filq/ui/navigation/BottomNavigation.kt
@@ -56,7 +56,7 @@ fun BottomNavigation(navController: NavHostController) {
         val navItemList = listOf(
             NavItem.Home,
             NavItem.Contact,
-            NavItem.QR,
+            NavItem.WiP,
             NavItem.More
         )
 

6ad50c5a729bf15fdee907cce7f1aa116e0b5b81
hyunjin0913 <lam103@naver.com>
2024-11-13 00:54:33 +0900

    chore: minor update

--- app/src/main/java/com/hongul/filq/MainActivity.kt
+++ app/src/main/java/com/hongul/filq/MainActivity.kt
@@ -3,45 +3,13 @@ package com.hongul.filq
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.hongul.filq.ui.theme.FilQTheme
+import com.hongul.filq.ui.theme.PhoneNumber.PhoneNumberScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            FilQTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            PhoneNumberScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FilQTheme {
-        Greeting("Android")
     }
 }

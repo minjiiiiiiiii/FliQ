e267def431597029caa88b334b0b3d47f70e21e5
slo0ey <rurchi1206@gmail.com>
2024-11-14 12:20:15 +0900

    feat: requestConnection, acceptConnection 추가
    - requestConnection, acceptConnection 함수 추가,
    - 저전력 모드로 동작하도록 변경
    - startAdvertising, startDiscovering함수를 async블럭이 아닌 launch블럭에서 동작하도록 변경

--- app/src/main/java/com/hongul/filq/api/NearbyApi.kt
+++ app/src/main/java/com/hongul/filq/api/NearbyApi.kt
@@ -4,31 +4,80 @@ import android.content.Context
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.AdvertisingOptions
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
+import com.google.android.gms.nearby.connection.ConnectionOptions
 import com.google.android.gms.nearby.connection.DiscoveryOptions
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback
+import com.google.android.gms.nearby.connection.PayloadCallback
 import com.google.android.gms.nearby.connection.Strategy
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 object NearbyApi {
-    fun CoroutineScope.startAdvertising(context: Context, name: String, callback: ConnectionLifecycleCallback) = async(Dispatchers.IO) {
+    fun CoroutineScope.startAdvertising(
+        context: Context,
+        name: String,
+        callback: ConnectionLifecycleCallback,
+        onSuccess: () -> Unit = {},
+        onFailure: () -> Unit = {}
+    ) = launch(Dispatchers.IO) {
         val option = AdvertisingOptions.Builder()
             .setStrategy(Strategy.P2P_STAR)
+            .setLowPower(true)
             .build()
 
         Nearby.getConnectionsClient(context)
             .startAdvertising(name, SERVICE_ID, callback, option)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure() }
     }
 
-    fun CoroutineScope.startDiscovering(context: Context, callback: EndpointDiscoveryCallback) = async(Dispatchers.IO) {
+    fun CoroutineScope.startDiscovering(
+        context: Context,
+        callback: EndpointDiscoveryCallback,
+        onSuccess: () -> Unit = {},
+        onFailure: () -> Unit = {}
+    ) = launch(Dispatchers.IO) {
         val option = DiscoveryOptions.Builder()
             .setStrategy(Strategy.P2P_STAR)
+            .setLowPower(true)
             .build()
 
         Nearby.getConnectionsClient(context)
             .startDiscovery(SERVICE_ID, callback, option)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure() }
+    }
+
+    fun CoroutineScope.requestConnection(
+        context: Context,
+        name: String,
+        id: String,
+        callback: ConnectionLifecycleCallback,
+        onSuccess: () -> Unit = {},
+        onFailure: () -> Unit = {}
+    ) = launch(Dispatchers.IO) {
+        val option = ConnectionOptions.Builder()
+            .setLowPower(true)
+            .build()
+
+        Nearby.getConnectionsClient(context)
+            .requestConnection(name, id, callback, option)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure() }
+    }
+
+    fun CoroutineScope.acceptConnection(
+        context: Context,
+        id: String,
+        callback: PayloadCallback,
+        onSuccess: () -> Unit = {},
+        onFailure: () -> Unit = {}
+    ) = launch(Dispatchers.IO) {
+        Nearby.getConnectionsClient(context)
+            .acceptConnection(id, callback)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure() }
     }
 
     fun CoroutineScope.stopAdvertising(context: Context) = launch(Dispatchers.IO) {

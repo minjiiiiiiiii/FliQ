df2e3ae2f872e45efc496b2740b726c9ecd6228a
slo0ey <rurchi1206@gmail.com>
2024-11-17 08:23:14 +0900

    feat: sdk 버전별 권한 리스트 추가

---
+++ app/src/main/java/com/hongul/filq/util/Permission.kt
@@ -0,0 +1,44 @@
+package com.hongul.filq.util
+
+import android.Manifest
+import android.os.Build
+
+val permissionList =
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+        listOfNotNull(
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_ADVERTISE,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.NEARBY_WIFI_DEVICES
+        )
+    else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+        listOfNotNull(
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_ADVERTISE,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+        listOfNotNull(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+    else
+        listOfNotNull(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        )

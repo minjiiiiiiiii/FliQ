3eb3cb5a48d4f36a340a84920b52e3b92ed35f16
slo0ey <rurchi1206@gmail.com>
2024-11-27 22:12:26 +0900

    feat: QR기능 추가

--- app/src/main/java/com/hongul/filq/ui/share/CardShareScreen.kt
+++ app/src/main/java/com/hongul/filq/ui/share/CardShareScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
@@ -50,7 +51,6 @@ data class CardShareRoute(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardShareScreen(
-    navigator: NavHostController,
     viewModel: CardShareViewModel = viewModel(
         factory = CardShareViewModelProvider.Factory,
     )
@@ -157,7 +157,7 @@ fun CardShareScreen(
 
                             AnimatedVisibility(showQR) {
                                 Image(
-                                    qrImage,
+                                    viewModel.qrCode.asImageBitmap(),
                                     contentDescription = "QR 코드",
                                     modifier = Modifier.fillMaxWidth(),
                                     contentScale = ContentScale.FillWidth

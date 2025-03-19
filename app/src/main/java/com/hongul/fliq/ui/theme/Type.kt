a2713c8a156bb110924e11151b6a830eb1d7a1b8
slo0ey <rurchi1206@gmail.com>
2025-03-19 16:32:02 +0900

    design: pretendard 폰트 추가

--- app/src/main/java/com/hongul/fliq/ui/theme/Type.kt
+++ app/src/main/java/com/hongul/fliq/ui/theme/Type.kt
@@ -2,9 +2,13 @@ package com.hongul.fliq.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hongul.fliq.R
+
+val Pretendard = FontFamily(Font(R.font.pretendard))
 
 // Set of Material typography styles to start with
 val Typography = Typography(

17e5e637a8a93eb3aa3b4fd1b2226399bb7d0a03
slo0ey <rurchi1206@gmail.com>
2025-04-08 05:27:07 +0900

    refactor: MainActivity ui 패키지로 옮김

---
+++ app/src/main/java/com/hongul/fliq/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hongul.fliq.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.hongul.fliq.ui.theme.FliQTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FliQTheme(content = { FliQApp() })
+        }
+    }
+}

6ec983847d6f0e85949f2f0dcb762eda0aca7a86
slo0ey <rurchi1206@gmail.com>
2025-04-24 04:17:56 +0900

    feat: Card, User 모델 추가

--- app/src/main/java/com/hongul/fliq/model/card/Card.kt
+++ app/src/main/java/com/hongul/fliq/model/card/Card.kt
@@ -1,13 +1,46 @@
 package com.hongul.fliq.model.card
 
 import androidx.compose.runtime.Immutable
+import com.hongul.fliq.data.card.CardEntity
 
 @Immutable
 data class Card(
-    val id: Long,
-    val owner: Long,
-    val name: String,
+    val id: Int,
+    val owner: Int,
+    val name: String = "",
     val email: String = "",
     val contact: String = "",
+    val profileImageURL: String,
+    val cardImageURL: String,
+    val organization: String,
+    val position: String,
     val private: Boolean
+) {
+    companion object {
+        fun fromEntity(entity: CardEntity) = Card(
+            id = entity.id,
+            owner = entity.owner,
+            name = entity.name,
+            email = entity.email,
+            contact = entity.contact,
+            profileImageURL = entity.profileImageURL,
+            cardImageURL = entity.cardImageURL,
+            organization = entity.organization,
+            position = entity.position,
+            private = entity.private
+        )
+    }
+}
+
+fun Card.toEntity() = CardEntity(
+    id = id,
+    owner = owner,
+    name = name,
+    email = email,
+    contact = contact,
+    profileImageURL = profileImageURL,
+    cardImageURL = cardImageURL,
+    organization = organization,
+    position = position,
+    private = private
 )
--- app/src/main/java/com/hongul/fliq/model/user/User.kt
+++ app/src/main/java/com/hongul/fliq/model/user/User.kt
@@ -1,5 +1,28 @@
 package com.hongul.fliq.model.user
 
+import androidx.compose.runtime.Immutable
+import com.hongul.fliq.data.user.UserEntity
+
+@Immutable
 data class User(
-    val id: Long
+    val id: Int,
+    val name: String,
+    val email: String = "",
+    val profileImageURL: String = ""
+) {
+    companion object {
+        fun fromEntity(entity: UserEntity) = User(
+            id = entity.id,
+            name = entity.name,
+            email = entity.email,
+            profileImageURL = entity.profileImageURL
+        )
+    }
+}
+
+fun User.toEntity() = UserEntity(
+    id = id,
+    name = name,
+    email = email,
+    profileImageURL = profileImageURL
 )
